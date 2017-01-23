8d0acae4e51583f301e1420bc735d7f1bc043774
Gabriel Ramirez <gabrielramirez1109@gmail.com>
2017-01-23 01:09:49 -0800

    completing assignment2.11

--- scripts/album.js
+++ scripts/album.js
@@ -12,6 +12,27 @@
          { title: 'Magenta', duration: '2:15'}
      ]
  };
+ var albumMine = {
+     title: 'The Rise and Fall of Ziggy Stardust and the Spiders from Mars',
+     artist: 'David Bowie',
+     label: 'Rock',
+     year: '1972',
+     albumArtUrl: 'assets/images/album_covers/ziggy.jpg',
+     songs: [
+         { title: 'Five Years', duration: '4:42' },
+         { title: 'Soul Love', duration: '3:34' },
+         { title: 'Moonage Daydream', duration: '4:40' },
+         { title: 'Starman', duration: '4:10'},
+         { title: 'It A\'int Easy', duration: '2:58'},
+         { title: 'Lady Stardust', duration: '3:22'},
+         { title: 'Star', duration: '2:47'},
+         { title: 'Hang On to Yourself', duration: '2:40'},
+         { title: 'Ziggy Stardust', duration: '3:13'},
+         { title: 'Suffragate City', duration: '3:25'},
+         { title: 'Rock \'n\' Roll Suicide', duration: '2:58'}
+     ]
+ };
+
  
  // Another Example Album
  var albumMarconi = {
@@ -64,7 +85,22 @@ var setCurrentAlbum = function(album) {
  };
  
  window.onload = function() {
-     setCurrentAlbum(albumPicasso);
+     setCurrentAlbum(albumMine);
  };
 
-//comment to update last commit
+document.getElementsByClassName('album-cover-art')[0].addEventListener("click", function(){
+  var currentImage = document.getElementsByClassName('album-cover-art')[0].getAttribute('src');
+  var nextImage;
+  //if picasso, switch to marconi
+  if (currentImage === "assets/images/album_covers/01.png")
+    setCurrentAlbum(albumMarconi);
+  
+  //if marconi, switch to mine  
+  else if (currentImage === "assets/images/album_covers/20.png")  
+    setCurrentAlbum(albumMine);
+  
+  // can only be mine otherwise, in this case go back to picasso
+  else
+    setCurrentAlbum(albumPicasso);
+  
+})
