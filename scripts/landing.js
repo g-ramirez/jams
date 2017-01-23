cb2d0597936c7f26afd8edc72d94bfc940475994
Gabriel Ramirez <gabrielramirez1109@gmail.com>
2017-01-22 19:26:07 -0800

    Completing checkpoint 2.9

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,18 +1,32 @@
-var animatePoints = function() {
+var pointsArray = document.getElementsByClassName('point');
 
-  var points = document.getElementsByClassName('point');
+var animatePoints = function(points) {
+
      
        
-    //scaleX changed from 1 to 2   
+    //scaleX changed back to 1 for better readability
    var revealPoint = function(index) {
       points[index].style.opacity = 1;
-      points[index].style.transform = "scaleX(2) translateY(0)";
-      points[index].style.msTransform = "scaleX(2) translateY(0)";
-      points[index].style.WebkitTransform = "scaleX(2) translateY(0)";
-     }
+      points[index].style.transform = "scaleX(1) translateY(0)";
+      points[index].style.msTransform = "scaleX(1) translateY(0)";
+      points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+     };
        
     for (var i = 0; i < points.length; i++) {
       revealPoint(i);    
     }
     
  };
+
+// won't execute until all elements are completely lload
+window.onload = function() {
+     if (window.innerHeight > 950) {
+       animatePoints(pointsArray);
+     }
+  
+     window.addEventListener("scroll", function(event) {
+       if (pointsArray[0].getBoundingClientRect().top <= 500) {
+         animatePoints(pointsArray);
+       }
+     });
+}
