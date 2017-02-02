1a07507a0a3f4b8572f75eca61a7441de93cc467
Gabriel Ramirez <gabrielramirez1109@gmail.com>
2017-02-01 23:47:00 -0800

    completng checkpoint2.16

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,32 +1,26 @@
-var pointsArray = document.getElementsByClassName('point');
-
-var animatePoints = function(points) {
-
-     
-       
-    //scaleX changed back to 1 for better readability
-   var revealPoint = function(index) {
-      points[index].style.opacity = 1;
-      points[index].style.transform = "scaleX(1) translateY(0)";
-      points[index].style.msTransform = "scaleX(1) translateY(0)";
-      points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-     };
-       
-    for (var i = 0; i < points.length; i++) {
-      revealPoint(i);    
-    }
-    
+ var animatePoints = function() {
+      var revealPoint = function() {
+           // #7
+           $(this).css({
+               opacity: 1,
+               transform: 'scaleX(1) translateY(0)'
+           });
+       };
+   
+     $.each($('.point'), revealPoint);
  };
 
 // won't execute until all elements are completely lload
-window.onload = function() {
-     if (window.innerHeight > 950) {
-       animatePoints(pointsArray);
+$(window).load(function() {
+      if ($(window).height() > 950) {
+         animatePoints();
      }
-  
-     window.addEventListener("scroll", function(event) {
-       if (pointsArray[0].getBoundingClientRect().top <= 500) {
-         animatePoints(pointsArray);
-       }
-     });
-}
+       var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+
+      $(window).scroll(function(event) {
+        if ($(window).scrollTop() >= scrollDistance) {
+             animatePoints();
+         }
+      });
+
+});
